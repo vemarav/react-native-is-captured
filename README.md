2e737d9ee90896d1810361d42aaaef2c6cafeb45
vemarav <vemarav@outlook.in>
2021-11-24 18:43:09 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-is-captured
+
+A Boolean value that indicates whether the system is actively cloning the screen to another destination.
+
+## Installation
+
+```sh
+npm install react-native-is-captured
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-is-captured";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativeiscaptured/IsCapturedModule.java
@@ -0,0 +1,34 @@
+package com.reactnativeiscaptured;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = IsCapturedModule.NAME)
+public class IsCapturedModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "IsCaptured";
+
+    public IsCapturedModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
